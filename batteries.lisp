97768f47169efd0db251c70af5b3116a016c09d7
pnathan <pnathan@vandals.uidaho.edu>
2012-08-18 23:36:46 -0600

    Removing defobject

--- batteries.lisp
+++ batteries.lisp
@@ -45,7 +45,6 @@
 	   :chomp
 	   :join-string
 
-	   :defobject
 	   :class-slots-symbols
 	   :object-to-hash
 	   :print-generic-object
@@ -485,111 +484,6 @@ Expects seq to be a sequence of strings"
 (with-running-unit-tests
     (expect "a-b-c" (join-string "-" '("a" "b" "c"))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Object creation macros
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun build-var (classname var)
-  (let ((variable-symbol (if (consp var)
-			     (first var)
-			     var))
-	(variable-init-val (if (consp var)
-			       (second var)
-			       nil)))
-  (list variable-symbol
-        :initform variable-init-val
-        :accessor (intern (concatenate 'string (string classname) "-"
-                                       (string variable-symbol)))
-        :initarg (intern (string variable-symbol) :keyword))))
-
-(with-running-unit-tests
-    (expect
-     '(BAR :INITFORM NIL :ACCESSOR FOO-BAR :INITARG :BAR)
-     (build-var 'foo 'bar))
-
-  (expect
-   '(BAR :INITFORM QUUX :ACCESSOR FOO-BAR :INITARG :BAR)
-   (build-var 'foo '(bar quux))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun build-varlist (classname varlist)
-   (loop for var in varlist
-         collect (build-var classname var)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro def-ez-class (name &optional varlist &key documentation superclasses)
-  (let ((docpair nil)
-	(vars (loop for var in varlist
-		 collect (build-var name var))))
-
-    (if documentation
-	(setf docpair (list :documentation documentation))
-	(setf docpair (list :documentation "")))
-
-    `(if ,superclasses
-	 ,(let ((classes (cadr superclasses)))
-	   `(defclass ,name
-	       ,classes
-	     ,vars
-	     ,docpair))
-	 ;;else
-	 (defclass ,name ()
-	   ,vars
-	   ,docpair))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro def-ez-class-ctor (name varlist)
-  "
-;(defun make-name (&key (bar nil))
-;(make-instance 'name :bar bar))"
-  (let ((initarg-list
-	 (loop for var in varlist collect
-	      (if (consp var)
-		  (list (first var) (second var))
-		  (list var nil))))
-	(ctor-args
-	 (flatten
-	  (loop for var in varlist
-	     collect
-	       (let ((var-symbol (if (consp var)
-				     (first var)
-				     var)))
-		     (list (intern (string var-symbol) :keyword) var-symbol))))))
-
-    ;;Add the kwarg notator
-    (if initarg-list
-	(push '&key initarg-list))
-
-     `(defun ,(intern (concatenate 'string "MAKE-" (string name)))
-	  ,initarg-list
-	(make-instance (quote ,name)
-		       ,@ctor-args))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Note: this macro makes for a good deal less typing for your
-;; 'average' POD class structure. It's similar to DEFSTRUCT, but
-;; instead is a "normal" CLOS object.
-(defmacro defobject (name varlist &key (documentation nil) (superclasses nil))
-  "Defines a class `name`
-
-`name` will have its variables with these settings:
-  initform as nil (or specified)
-  accessor function as `name-var`
-  initarg as :var
-
-If a var is passed in as a pair (var val), val will become the
-initform.
-
-A make-`name` function definition will spring into existance
-
-Example:
-;(defobject world (population-normals population-wizards population-dragons)
-  :documentation ''Fun place!'')"
-
-  `(progn
-     (def-ez-class ,name ,varlist :documentation ,documentation :superclasses ,superclasses)
-     (def-ez-class-ctor ,name ,varlist)))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; CLOS manipulation routines
 
@@ -996,4 +890,3 @@ Other conditions beside `expected-errors` will exit out of this macro"
 		       (funcall ,fail-function))
 		     (go ,start-tag)))))
 	,result)))
-
